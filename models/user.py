b14af6adb1c9f07b441bc8abe20e695d4340f048
LeulBayesa <leulbayesa@gmail.com>
2022-10-30 15:08:29 -0700

    pycodestyle

--- models/user.py
+++ models/user.py
@@ -1,14 +1,9 @@
-#!/usr/bin/python3
-"""Module user.py
-that inherits from BaseModel.
-"""
-
+#!usr/bin/python3
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """class representing a User."""
-
+    """Creates a new user"""
     email = ""
     password = ""
     first_name = ""
